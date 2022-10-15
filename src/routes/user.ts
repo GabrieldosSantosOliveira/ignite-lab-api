4c8e92c29f152311b0faa7b17be7e8cafd7e3ec2
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-15 16:50:37 -0400

    feat: Add routes user

---
+++ src/routes/user.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { User } from "./../controllers/User";
+import { Auth } from "./../controllers/Auth";
+import { ValidateBody } from "./../middleware/ValidateBody";
+const routerUser = Router();
+routerUser.use(Auth.middlewareAuth);
+routerUser.get("/user/:id", User.findOne);
+routerUser.get("/user", User.findAll);
+routerUser.put("/user/:id", ValidateBody.UpdateUser, User.update);
+routerUser.delete("/user/:id", User.delete);
+export { routerUser };
