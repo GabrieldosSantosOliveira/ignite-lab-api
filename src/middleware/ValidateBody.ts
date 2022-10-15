77c5e12fe93a0fa56864671f1cc61ac3357b1ed1
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-15 16:49:52 -0400

    feat: Add middleware ValidateBody

---
+++ src/middleware/ValidateBody.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { User as UserType } from "@prisma/client";
+import { UserSchema, UserSchemaUpdate } from "./../models/User";
+export class ValidateBody {
+  static async CreateUser(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    const value: UserType = request.body;
+    const { error } = UserSchema.validate(value);
+    if (error) {
+      return response.status(400).json({ message: error.message });
+    }
+    return next();
+  }
+  static async UpdateUser(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    const value: UserType = request.body;
+    const { error } = UserSchemaUpdate.validate(value);
+    if (error) {
+      return response.status(400).json({ message: error.message });
+    }
+    return next();
+  }
+}
