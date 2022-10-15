4299a648ac82cc8aec9faff22a28678e0144dee8
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-15 16:47:54 -0400

    feat: Add new controller User

---
+++ src/controllers/User.ts
@@ -0,0 +1,42 @@
+import { prismaClient } from "./../database/prismaClient";
+import { User as UserType } from "@prisma/client";
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+class User {
+  static async create(request: Request, response: Response) {
+    const { email, firstName, lastName, password }: UserType = request.body;
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const user = await prismaClient.user.create({
+      data: { email, firstName, lastName, password: encryptedPassword },
+    });
+    return response.json(user);
+  }
+  static async findOne(request: Request, response: Response) {
+    const { id } = request.params;
+    const user = await prismaClient.user.findUnique({
+      where: { id },
+    });
+    return response.json(user);
+  }
+  static async findAll(request: Request, response: Response) {
+    const users = await prismaClient.user.findMany();
+    return response.json(users);
+  }
+  static async update(request: Request, response: Response) {
+    const { id } = request.params;
+    const { email, firstName, lastName, password }: UserType = request.body;
+    const user = await prismaClient.user.update({
+      where: { id },
+      data: { email, firstName, lastName, password },
+    });
+    return response.json(user);
+  }
+  static async delete(request: Request, response: Response) {
+    const { id } = request.params;
+    const user = await prismaClient.user.delete({
+      where: { id },
+    });
+    return response.json(user);
+  }
+}
+export { User };
