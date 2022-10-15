6bdca27be389abfb2880fe4274f7a01fa0e9ee0b
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-15 16:46:26 -0400

    feat: Add new routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
-
+import { routerUser } from "./routes/user";
+import { routerAuth } from "./routes/auth";
 import { config } from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
 config();
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(routerAuth);
+app.use(routerUser);
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
